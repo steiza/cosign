fca87b0fef1ef1179f25b635b5702510e43e9de1
Zach Steindler <steiza@github.com>
2025-10-06 11:44:03 -0400

    Remove unused import
    Signed-off-by: Zach Steindler <steiza@github.com>

--- test/helpers.go
+++ test/helpers.go
@@ -57,7 +57,6 @@ import (
 	"github.com/sigstore/cosign/v3/pkg/cosign"
 	"github.com/sigstore/cosign/v3/pkg/cosign/env"
 	ociremote "github.com/sigstore/cosign/v3/pkg/oci/remote"
-	sigs "github.com/sigstore/cosign/v3/pkg/signature"
 	v1 "github.com/sigstore/protobuf-specs/gen/pb-go/common/v1"
 	"github.com/sigstore/sigstore/pkg/signature"
 )
